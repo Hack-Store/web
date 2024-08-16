4742a2358a731f4455baa73af5ece22a7bb4a67b
arnav-kr <72879799+arnav-kr@users.noreply.github.com>
2024-08-16 16:33:23 +0530

    fix: make site static generatable

--- src/app/package/[package_id]/page.tsx
+++ src/app/package/[package_id]/page.tsx
@@ -3,28 +3,44 @@ import AppList from "@/components/AppList";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 
-export default function Package() {
+// Temperoraly added for static generation
+// TODO: Remove this later
+export async function generateStaticParams(): Promise<
+  [{ package_id: string }]
+> {
+  return [
+    {
+      package_id: "com.mycoolapp",
+    },
+  ];
+}
+
+export default function Package({
+  params,
+}: {
+  params: { package_id: string };
+}) {
+  const { package_id } = params;
+  console.log(package_id);
   return (
     <>
       <Header />
       <main className="min-h-screen pb-12 flex flex-col md:flex-row">
-        <section className="flex-1">
-
-        </section>
+        <section className="flex-1"></section>
         <aside>
-        <AppList className="md:flex-col md:mt-36">
-          {Array(5)
-            .fill(0)
-            .map((_, i) => (
-              <AppItemCard
-                key={i}
-                src="/placeholder.svg"
-                name="My Cool App"
-                ratings={4.5}
-                href="/package/com.mycoolapp"
-              />
-            ))}
-        </AppList>
+          <AppList className="md:flex-col md:mt-36">
+            {Array(5)
+              .fill(0)
+              .map((_, i) => (
+                <AppItemCard
+                  key={i}
+                  src="/placeholder.svg"
+                  name="My Cool App"
+                  ratings={4.5}
+                  href="/package/com.mycoolapp"
+                />
+              ))}
+          </AppList>
         </aside>
       </main>
       <Footer />
