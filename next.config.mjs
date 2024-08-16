9bacefd3b09f74188c9f32ea32650cbb79b1fbd4
arnav-kr <72879799+arnav-kr@users.noreply.github.com>
2024-08-16 18:15:55 +0530

    revert

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Github Pages
-  basePath: process.env.GITHUB_PAGES ? "/web" : undefined,
-  assetPrefix: process.env.GITHUB_PAGES ? "/web/" : undefined,
-};
+const nextConfig = {};
 
 export default nextConfig;
