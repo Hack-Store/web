e263d2767dff9ea9cb53eaf6bcea952863a98b74
arnav-kr <72879799+arnav-kr@users.noreply.github.com>
2024-08-16 18:05:47 +0530

    try+fix: static asset paths

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: process.env.GITHUB_PAGES ? "/web" : "/", // Github Pages
+};
 
 export default nextConfig;
